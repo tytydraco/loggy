a482470a208c546aee4bfb5f92237623e2c52c09
Tyler Nijmeh <tylernij@gmail.com>
2023-04-11 17:39:48 -0700

    Configurable timestamp
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/screens/new/new_screen.dart
+++ lib/src/screens/new/new_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:loggy/src/data/constants.dart';
 import 'package:loggy/src/widgets/rating_toggle_group.dart';
 
@@ -12,6 +13,41 @@ class NewScreen extends StatefulWidget {
 }
 
 class _NewScreenState extends State<NewScreen> {
+  DateTime date = DateTime.now();
+  final dateStringFormatter = DateFormat.yMMMMd().add_jm();
+
+  Future<DateTime?> _selectTimestamp(DateTime initial) async {
+    final newDate = await showDatePicker(
+      context: context,
+      initialDate: date,
+      firstDate: DateTime(0),
+      lastDate: DateTime.now(),
+    );
+
+    if (newDate == null) return null;
+
+    if (!context.mounted) return null;
+
+    final newTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(date),
+    );
+
+    if (newTime == null) return null;
+
+    final selectedDate = DateTime(
+      newDate.year,
+      newDate.month,
+      newDate.day,
+      newTime.hour,
+      newTime.minute,
+    );
+
+    if (selectedDate.isAfter(DateTime.now())) return null;
+
+    return selectedDate;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,11 +57,31 @@ class _NewScreenState extends State<NewScreen> {
       body: Padding(
         padding: const EdgeInsets.all(8),
         child: Column(
-          children: const [
-            Center(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: RatingToggleGroup(defaultRatingScale),
+          children: [
+            // Date and time.
+            OutlinedButton(
+              onPressed: () async {
+                final newDate = await _selectTimestamp(date);
+                if (newDate != null) {
+                  setState(() {
+                    date = newDate;
+                  });
+                }
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(dateStringFormatter.format(date)),
+              ),
+            ),
+
+            // Rating selection.
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: RatingToggleGroup(defaultRatingScale),
+                ),
               ),
             ),
           ],
