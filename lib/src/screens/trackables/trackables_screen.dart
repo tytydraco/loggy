c197c69fb581c565dca580b60c34005a0d89f643
Tyler Nijmeh <tylernij@gmail.com>
2023-04-13 16:40:21 -0700

    Add and edit trackables
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/screens/trackables/trackables_screen.dart
+++ lib/src/screens/trackables/trackables_screen.dart
@@ -16,8 +16,43 @@ class _TrackablesScreenState extends State<TrackablesScreen>
     with AutomaticKeepAliveClientMixin {
   late final _storage = context.read<StorageBase>();
 
-  Future<void> _addNewTrackable() async {
-    // TODO(tytydraco): add trackables and display existing ones
+  Future<void> _editTrackable({String? initialTrackable}) async {
+    final newTrackable = await showDialog<String?>(
+      context: context,
+      builder: (context) {
+        final editController = TextEditingController();
+        if (initialTrackable != null) editController.text = initialTrackable;
+
+        return AlertDialog(
+          title: const Text('Edit'),
+          content: TextField(
+            controller: editController,
+            onSubmitted: (text) => Navigator.pop(context, text),
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, editController.text),
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (newTrackable == null || newTrackable == initialTrackable) return;
+
+    if (initialTrackable != null) {
+      await _storage.deleteTrackable(initialTrackable);
+    }
+
+    await _storage.addTrackable(newTrackable);
   }
 
   @override
@@ -34,8 +69,9 @@ class _TrackablesScreenState extends State<TrackablesScreen>
             final trackables = snapshot.data ?? {};
             return TrackablesList(
               trackables: trackables,
-              onTap: (trackable) {
-                // TODO(tytydraco): Edit
+              onTap: (trackable) async {
+                await _editTrackable(initialTrackable: trackable);
+                setState(() {});
               },
             );
           } else {
@@ -46,7 +82,10 @@ class _TrackablesScreenState extends State<TrackablesScreen>
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async => _addNewTrackable(),
+        onPressed: () async {
+          await _editTrackable();
+          setState(() {});
+        },
         tooltip: 'New',
         child: const Icon(Icons.edit),
       ),
