fd4e14bc91669fab03c2fa110272a3d9fcf3e10e
Tyler Nijmeh <tylernij@gmail.com>
2024-07-27 17:17:34 -0700

    Calculate correlation coefficients
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/screens/analysis/analysis_screen.dart
+++ lib/src/screens/analysis/analysis_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loggy/src/data/storage_base.dart';
+import 'package:loggy/src/utils/trackables_correlations.dart';
 import 'package:provider/provider.dart';
 
 /// Perform analysis for entries.
@@ -21,7 +22,18 @@ class _AnalysisScreenState extends State<AnalysisScreen>
       appBar: AppBar(
         title: const Text('Analysis'),
       ),
+      body: Center(
+        child: OutlinedButton(
+          onPressed: _calculateCorrelations,
+          child: const Text('Test'),
+        ),
+      ),
     );
+  }
+
+  Map<String, double> _calculateCorrelations() {
+    final trackablesCorrelations = TrackablesCorrelations(_storage.entries);
+    return trackablesCorrelations.calculateAll(_storage.trackables);
   }
 
   @override
---
+++ lib/src/utils/trackables_correlations.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+import 'package:loggy/src/data/constants.dart';
+import 'package:loggy/src/models/entry.dart';
+
+/// Calculate correlations of data.
+class TrackablesCorrelations {
+  /// Creates a new [TrackablesCorrelations].
+  TrackablesCorrelations(this.entries);
+
+  /// Entries to use.
+  final Iterable<Entry> entries;
+
+  double _stdDev(List<int> nums) {
+    final mean = nums.average;
+    final squaredDiffs = nums.map((e) => pow(e - mean, 2)).toList();
+    final variance = squaredDiffs.sum / nums.length;
+    return sqrt(variance);
+  }
+
+  /// Calculate the correlation coefficient for a single trackable.
+  double calculateFor(String trackable) {
+    final x = entries
+        .map((e) => (e.trackables?.contains(trackable) ?? false) ? 1 : 0)
+        .toList();
+    final xMean = x.average;
+    final xStdDev = _stdDev(x);
+    final stdXs = x.map((e) => (e - xMean) / xStdDev).toList();
+
+    final y = entries
+        .map((e) => e.rating.value / defaultRatingScale.last.value)
+        .toList();
+    final yMean = y.average;
+    final yStdDev = _stdDev(x);
+    final stdYs = y.map((e) => (e - yMean) / yStdDev).toList();
+
+    final stdZs = Map.fromIterables(stdXs, stdYs)
+        .entries
+        .map((e) => e.value * e.key)
+        .toList();
+
+    return stdZs.sum / (stdZs.length - 1);
+  }
+
+  /// Calculate the correlation coefficient for all trackables.
+  Map<String, double> calculateAll(Iterable<String> trackables) {
+    return Map.fromIterable(
+      trackables,
+      value: (e) => calculateFor(e as String),
+    );
+  }
+}
