94881d397d6f63077c6cfd49283b8f359e5909a6
Tyler Nijmeh <tylernij@gmail.com>
2024-09-30 14:50:49 -0700

    Remove dead code
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/utils/list_export_import/list_export_import.dart
+++
@@ -1,3 +0,0 @@
-export 'list_export_import_unsupported.dart'
-    if (dart.library.io) 'list_export_import_common.dart'
-    if (dart.library.html) 'list_export_import_web.dart';
--- lib/src/utils/list_export_import/list_export_import_unsupported.dart
+++
@@ -1,9 +0,0 @@
-import 'package:loggy/src/models/loggy_list.dart';
-
-/// Exports the [list] as a JSON file.
-Future<void> exportListToFile(LoggyList list) =>
-    throw UnsupportedError('Unsupported platform');
-
-/// Imports a JSON file as a list.
-Future<LoggyList?> importFileAsList() =>
-    throw UnsupportedError('Unsupported platform');
